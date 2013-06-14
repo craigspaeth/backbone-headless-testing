095e34c8d7def0a2857b94a8f5a624cae1430d10
Craig Spaeth <craigspaeth@gmail.com>
2013-06-14 16:13:55 -0400

    done

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # backbone-headless-testing
 
-An example/bootstrap project that shows how to test a backbone app headlessly.
+An example/bootstrap project that shows how to headlessly test a simple backbone app.
 
 ## Getting Started
 
 1. Install [node.js](http://nodejs.org/)
 2. Install npm modules `npm install`
+3. Run the app `node app.js`
+4. Run the tests `make test`
--- test/integration/todos.js
+++ test/integration/todos.js
@@ -1,18 +1,36 @@
 var Browser = require('zombie'),
-    app = require('../../app');
+    app = require('../../app'),
+    sinon = require('sinon'),
+    _ = require('underscore');
 
 describe('Adding a todo and completing it', function() {
   
-  var browser;
+  var browser, $, ajaxSpy;
   
   before(function(done) {
     app.listen('5000', function() {
       console.log('Test server listening on 5000');
-      Browser.visit('http://localhost:5000')
+      Browser.visit('http://localhost:5000', function(err, b) {
+        browser = b;
+        $ = browser.window.$;
+        ajaxSpy = sinon.spy($, 'ajax');
+        browser.wait(function(){
+          browser.window.$(function(){
+            done();
+          });
+        });
+      });
     });
   });
   
-  it('Adds the todo, renders the todos, and crosses it out when done', function() {
-    
+  it('Adds the todo, renders the todos, and crosses it out when done', function(done) {
+    $('#add-todo').val('Foo').change();
+    browser.wait(function() {
+      $('#todos li').length.should.be.above(2);
+      $('#todos li:last-child .complete-todo').click();
+      JSON.parse(_.last(ajaxSpy.args)[0].data).completed.should.be.ok;
+      $('#todos li:last-child').hasClass('completed').should.be.ok
+      done()
+    });
   });
 });
